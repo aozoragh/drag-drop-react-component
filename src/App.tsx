6df15a010348d1d8918aa44f76baac32242c2c25
apollo110217 <popescufv1021@gmail.com>
2023-07-03 07:59:01 +0200

    add: new react component - Drag & Drop Area.

---
+++ src/App.tsx
@@ -0,0 +1,85 @@
+import React, { FC, ReactElement, useRef } from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+
+interface ModalProps {
+  isVisible: Boolean;
+  title: String;
+  content: ReactElement;
+  footer: ReactElement;
+  onClose: Function;
+}
+
+const FileDrop = ({ onDrop }) => {
+  const [drag, setDrag] = React.useState(false);
+  const [filename, setFilename] = React.useState("");
+  let dropRef = React.createRef();
+  let dragCounter = 0;
+
+  const handleDrag = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
+
+  const handleDragIn = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    dragCounter++;
+    if (e.dataTransfer.items && e.dataTransfer.items.length > 0) setDrag(true);
+  };
+
+  const handleDragOut = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    dragCounter--;
+    if (dragCounter === 0) setDrag(false);
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setDrag(false);
+    if (e.dataTransfer.files && e.dataTransfer.files.length > 0) {
+      onDrop(e.dataTransfer.files[0]);
+      setFilename(e.dataTransfer.files[0].name);
+      e.dataTransfer.clearData();
+      dragCounter = 0;
+    }
+  };
+
+  React.useEffect(() => {
+    let div = dropRef.current;
+    div.addEventListener("dragenter", handleDragIn);
+    div.addEventListener("dragleave", handleDragOut);
+    div.addEventListener("dragover", handleDrag);
+    div.addEventListener("drop", handleDrop);
+    return () => {
+      div.removeEventListener("dragenter", handleDragIn);
+      div.removeEventListener("dragleave", handleDragOut);
+      div.removeEventListener("dragover", handleDrag);
+      div.removeEventListener("drop", handleDrop);
+    };
+  });
+
+  return (
+    <div
+      ref={dropRef}
+      className={
+        drag ? "filedrop drag" : filename ? "filedrop ready" : "filedrop"
+      }
+    >
+      {filename && !drag ? <div>{filename}</div> : <div>Drop a file here!</div>}
+    </div>
+  );
+};
+
+const App = () => {
+  const [isModal, setModal] = React.useState(false);
+  return (
+    <>
+      <FileDrop onDrop={console.log} />
+    </>
+  );
+};
+
+export default App;
